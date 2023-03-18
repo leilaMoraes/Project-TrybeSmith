55fec44a87ca2a1836bfbc57c31c54344b491459
Leila Moraes <leila.violino@gmail.com>
2023-03-18 14:34:02 -0300

    feat: rota para exibir todos os produtos

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -6,5 +6,6 @@ const productsRouter = Router();
 const controller = new ProductsController();
 
 productsRouter.post('/', controller.create);
+productsRouter.get('/', controller.getAll);
 
 export default productsRouter;
