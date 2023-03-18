15327ffaeb89657ecfa19eb3fe07a8d8bfbc767c
Leila Moraes <leila.violino@gmail.com>
2023-03-18 15:43:58 -0300

    feat: controller para cadastrar um usuário

---
+++ src/controllers/usersController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import UsersService from '../services/usersService';
+
+export default class ProductsController {
+  service: UsersService;
+
+  constructor() {
+    this.service = new UsersService();
+  }
+
+  create = async (req: Request, res: Response) => {
+    const { type, message } = await this.service.create(req.body);
+    return res.status(type).json(message);
+  };
+}
