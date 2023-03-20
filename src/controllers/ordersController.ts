74ed34474afce6c3f843aadf040800c170174d3f
Leila Moraes <leila.violino@gmail.com>
2023-03-20 01:50:28 -0300

    feat: controller para exibir todos os pedidos

---
+++ src/controllers/ordersController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import OrdersService from '../services/ordersService';
+
+export default class ProductsController {
+  service: OrdersService;
+
+  constructor() {
+    this.service = new OrdersService();
+  }
+
+  getAll = async (_req: Request, res: Response) => {
+    const { type, message } = await this.service.getAll();
+    return res.status(type).json(message);
+  };
+}
